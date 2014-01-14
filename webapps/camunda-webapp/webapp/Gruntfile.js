81b5d8e52fb4ead4a904e523f70fdbb355f95c84
Valentin Vago <zeropaper@irata.ch>
2014-01-14 11:10:15 +0100

    feat(grunt.js) prepare grunt.js build tool

--- webapps/camunda-webapp/webapp/Gruntfile.js
+++ webapps/camunda-webapp/webapp/Gruntfile.js
@@ -1,36 +1,40 @@
+var path = require('path');
+var fs = require('fs');
+var _ = require('underscore');
+
 module.exports = function(grunt) {
   var packageJSON = grunt.file.readJSON('package.json');
+  // var requireConf = require('./src/main/webapp/requirejs-conf');
 
   // Project configuration.
   grunt.initConfig({
     pkg: packageJSON,
 
     build: {
-      options: packageJSON.camundaBpm,
       production: {},
       development: {}
     },
 
-    clean: [
-      // 'src/main/webapp/app/testplugin/app/views',
-      // 'src/main/webapp/app/testplugin/app'
-    ],
+    clean: {
+      target: [
+        'target/webapp'
+      ]
+    },
 
     copy: {
-      production: {},
       development: {
         files: [
           {
             expand: true,
-            cwd: 'src/main/webapp/app/',
-            src: ['{,**/}*.js'],
-            dest: 'src/main/webapp/app/testplugin/app/'
+            cwd: 'src/main/webapp/WEB-INF',
+            src: ['*'],
+            dest: 'target/webapp/WEB-INF'
           },
           {
             expand: true,
-            cwd: 'frontend/templates/',
-            src: ['{,**/}*.html'],
-            dest: 'src/main/webapp/app/testplugin/app/views/'
+            cwd: 'src/main/webapp/',
+            src: ['{app,assets,plugin,develop}/{,**/}*.{js,html,jpg,png,gif}'],
+            dest: 'target/webapp/'
           }
         ]
       }
@@ -41,61 +45,83 @@ module.exports = function(grunt) {
         livereload: false
       },
 
+      // watch for source script changes
       scripts: {
         files: [
           'Gruntfile.js',
-          'src/main/webapp/app/**/*.js'
+          'src/main/webapp/{app,assets,develop,plugin}/**/*.js'
         ],
         tasks: [
-          'build:development'
+          // 'jshint:scripts',
+          'copy:development'
         ]
       },
 
+      // watch for source script and test changes
       tests: {
-        files: [
-          'src/test/js/**/*.js'
+        files: [,
+          'src/main/webapp/{app,assets,develop,plugin}/**/*.js',
+          'src/test/js/{config,e2e,unit}/{,**/}*.js'
         ],
         tasks: [
-          'jshint:test',
-          'karma:development'
+          // 'jshint:test',
+          // we use the CI versions (who are runned only once)
+          'karma:unitOnce',
+          'karma:e2eOnce'
+        ]
+      },
+
+      styles: {
+        files: [
+          'src/main/webapp/scss/{**/,}*.{scss,sass}'
+        ],
+        tasks: [
+          'compass:development'
         ]
       },
 
       servedAssets: {
         options: {
-          libvereload: true
+          livereload: true
         },
         files: [
-          'src/main/webapp/app/testplugin/app/**/*.{js,jpg,png,gif,html}'
+          'target/webapp/{app,plugin,develop,assets}/**/*.{js,jpg,png,gif,html}'
         ],
         tasks: []
       }
     },
 
-    jshint: {
-      options: {
-        browser: true,
-        globals: {
-          angular: true,
-          jQuery: true
-        }
-      },
-      scripts: {
-        files: {
-          src: [
-            'Gruntfile.js',
-            // 'src/main/webapp/app/testplugin/app/**/*.js',
-            'src/main/webapp/app/**/*.js'
-          ]
-        }
-      }
-    },
+    // jshint: {
+    //   options: {
+    //     browser: true,
+    //     globals: {
+    //       angular: true,
+    //       jQuery: true
+    //     }
+    //   },
+    //   test: {
+    //     files: {
+    //       src: [
+    //         'test/js/{config,e2e,unit}/{,**/}*.js'
+    //       ]
+    //     }
+    //   },
+    //   scripts: {
+    //     files: {
+    //       src: [
+    //         'Gruntfile.js',
+    //         'src/main/webapp/{app,assets,develop,plugin}/{,**/}*.js'
+    //       ]
+    //     }
+    //   }
+    // },
 
     karma: {
       options: {
         singleRun: true,
         browsers: ['Chrome', 'Firefox', 'IE']
       },
+
       unit: {
         configFile: 'src/test/js/config/karma.unit.js'
       },
@@ -104,57 +130,160 @@ module.exports = function(grunt) {
       },
 
       //continuous integration mode: run tests once in PhantomJS browser.
-      ciUnit: {
+      unitOnce: {
         singleRun: true,
-        configFile: 'src/test/js/config/karma.unit.js'
+        configFile: 'src/test/js/config/karma.unit.js',
         browsers: ['PhantomJS']
-      }
-      ciE2e: {
+      },
+      e2eOnce: {
         singleRun: true,
-        configFile: 'src/test/js/config/karma.e2e.js'
+        configFile: 'src/test/js/config/karma.e2e.js',
         browsers: ['PhantomJS']
       }
     },
 
-    jsdoc : {
-      dist : {
-        src: [
-          'src/main/webapp/**/*.js',
-          'src/test/js/{unit,e2e}/**/*.js'
-        ],
-        options: {
-          destination: 'doc'
-        }
-      }
-    }
+    // jsdoc : {
+    //   dist : {
+    //     src: [
+    //       'src/main/webapp/**/*.js',
+    //       'src/test/js/{unit,e2e}/**/*.js'
+    //     ],
+    //     options: {
+    //       destination: 'doc'
+    //     }
+    //   }
+    // },
+
+    // compass: {
+    //   options: {
+    //     sassDir: 'src/main/webapp/scss',
+    //     cssDir: 'target/webapp/assets/css'
+    //   },
+    //   production: {
+    //     options: {
+    //       environment: 'production'
+    //     }
+    //   },
+    //   development: {
+    //     options: {}
+    //   }
+    // },
+
+    // requirejs: {
+    // ngr: {
+    //   // see https://github.com/jrburke/r.js/blob/master/build/example.build.js
+    //   options: {
+    //     // appDir: 'src/main/webapp',
+
+    //     dir: 'target/webapp',
+
+    //     // Inlines the text for any text! dependencies, to avoid the separate
+    //     // async XMLHttpRequest calls to load those dependencies.
+    //     inlineText: true,
+
+    //     optimize: 'none',
+
+    //     paths: requireConf.paths,
+    //     shim: requireConf.shim,
+
+    //     // CommonJS packages support
+    //     // http://requirejs.org/docs/api.html#packages
+    //     packages: requireConf.packages,
+
+    //     //
+    //     optimizeCss: 'none'
+    //   },
+
+    //   app: {
+    //     modules: [{
+    //       name: 'src/main/webapp/app',
+    //       out: 'target/webapp/app/app.js',
+    //       override: {},
+    //       exclude: []
+    //     }]
+    //   },
+
+    //   admin: {
+    //     modules: [{
+    //       name: 'src/main/webapp/app/admin/admin',
+    //       out: 'target/webapp/app/admin.js',
+    //       override: {},
+    //       exclude: []
+    //     }]
+    //   },
+
+    //   cockpit: {
+    //     modules: [
+    //       {
+    //         name: 'cockpit',
+    //         // name: 'target/webapp/cockpit',
+    //         out: 'target/webapp/app/cockpit.js',
+    //         override: {},
+    //         exclude: []
+    //       }
+    //     ]
+    //   },
+
+    //   tasklist: {
+    //     modules: [{
+    //       name: 'src/main/webapp/app/tasklist/tasklist',
+    //       out: 'target/webapp/app/tasklist.js',
+    //       override: {},
+    //       exclude: []
+    //     }]
+    //   }
+    // }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-jsdoc');
+  // grunt.loadNpmTasks('grunt-contrib-compass');
+  // grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-karma');
+
+  // custom task for ngDefine minification
+  grunt.registerMultiTask('ngr', 'Minifies the angular related scripts', function() {
+    var done = this.async();
+    var ngr = require('requirejs-angular-define/src/ngr');
+
+    var setup = _.extend({}, this.options(), this.data);
+    // console.info('ngr options', setup);
+
+    ngr.optimize(setup, function() {
+      console.info('optimized', arguments.length);
+      done();
+    }, function(e) {
+      console.log('Error during minify: ', e);
+      done(new Error('With failures: ' + e));
+    });
+  });
 
   // Aimed to hold more complex build processes
   grunt.registerMultiTask('build', 'Build the frontend assets', function() {
     var tasks = [
-      'jshint',
-      // 'karma:'+ this.target,
       'clean'
     ];
 
     if (this.target === 'production') {
       tasks = tasks.concat([
-        // copy non-script files
-        // concat
-        // minify
+        // TODO: minification using ngr:
+        // - Minifaction: https://app.camunda.com/jira/browse/CAM-1667
+        // - Bug in ngDefine: https://app.camunda.com/jira/browse/CAM-1713
+        'copy:development'
       ]);
     }
     else {
-      tasks.push('copy:development');
+      tasks = tasks.concat([
+        'copy:development'
+      ]);
     }
+
+    tasks = tasks.concat([
+      'compass:'+ this.target
+    ]);
 
     grunt.task.run(tasks);
   });
